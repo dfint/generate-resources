a072405f78ccb244ea0cef246bc31a9109f29337
insolor <insolor@gmail.com>
2023-10-30 23:11:32 +0300

    Add a test

---
+++ tests/test_parse_downloads.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import pytest
+
+
+from parse_downloads_page import ReleaseInfo, base_url, parse_download_info
+
+tests_dir = Path(__file__).parent
+
+
+@pytest.fixture
+def downloads_page():
+    with open(tests_dir / "downloads.html") as file:
+        yield file.read()
+
+
+def test_parse_downloads_page(downloads_page):
+    result = parse_download_info(downloads_page)
+    assert result == ReleaseInfo(
+        name="DF 50.11 (October 3, 2023)",
+        classic_win_url=base_url + "df_50_11_win.zip",
+        classic_win_small_url=base_url + "df_50_11_win_s.zip",
+        linux_url=base_url + "df_50_11_linux.tar.bz2",
+    )
