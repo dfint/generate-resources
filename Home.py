72f4f53328b720fae9900bff2392879939feb246
insolor <insolor@gmail.com>
2023-10-30 22:00:50 +0300

    Rename files

--- Home.py
+++ Home.py
@@ -12,8 +12,6 @@ def get_latest_release():
     return parse_downloads_page.get_latest_release()
 
 
-st.write("Fetching latest release info...")
-
 latest_release_info = get_latest_release()
 
 st.write("Latest release:", latest_release_info.name)
